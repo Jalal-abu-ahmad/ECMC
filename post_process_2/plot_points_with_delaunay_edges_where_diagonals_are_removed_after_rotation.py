fixed angel rotation calculation

post_process_2/plot_points_with_delaunay_edges_where_diagonals_are_removed_after_rotation.py
@@ -8,17 +8,18 @@ from Structure import Metric
 
 def demo():
     # Generate a square lattice of points
-    L = 20  # Size of the square lattice
-    N = 100  # Number of points
-    noise = 0.1
-    rotation_angle_degree = 90
+    L = 200  # Size of the square lattice
+    N = 10000  # Number of points
+    noise = 0
+    rotation_angle_degree = 5
+    boundaries=([L,L])
+    epsilon= L/np.sqrt(N)/2
     rotation_angle = np.deg2rad(rotation_angle_degree)
     rotation_matrix = np.array([
         [np.cos(rotation_angle), np.sin(rotation_angle)],
-        [-np.sin(rotation_angle), np.cos(rotation_angle)],
-    ])
-    x = np.linspace(0, L, int(np.sqrt(N)))
-    y = np.linspace(0, L, int(np.sqrt(N)))
+        [-np.sin(rotation_angle), np.cos(rotation_angle)] ])
+    x = np.linspace(epsilon, L-epsilon, int(np.sqrt(N)))
+    y = np.linspace(epsilon, L-epsilon, int(np.sqrt(N)))
     xx, yy = np.meshgrid(x, y)
     points = np.array([xx.flatten(), yy.flatten()]).T
     rotated_points = points @ rotation_matrix
@@ -28,12 +29,12 @@ def demo():
     # Add some random defects to the lattice
     noise = np.random.normal(0, noise, size=rotated_points.shape)
     rotated_points += noise
-    aligned_points = align_points(rotated_points)
+    # utils.cyc_position_alignment(rotated_points, boundaries)
+    aligned_points = align_points(rotated_points, l_x=L, l_y=L, N=N)
 
+    utils.plot_points_with_delaunay_edges_where_diagonals_are_removed(points=aligned_points, L=L, N=N)
 
-    utils.plot_points_with_delaunay_edges_where_diagonals_are_removed(points=aligned_points,L=L,N=N)
-
-    utils.plot_points_with_delaunay_edges_where_diagonals_are_removed(points=rotated_points,L=L,N=N)
+    utils.plot_points_with_delaunay_edges_where_diagonals_are_removed(points=rotated_points, L=L, N=N)
 
 
 def calculate_rotation_angle(points ,a):
@@ -55,18 +56,19 @@ def calculate_rotation_angel_averaging_on_all_sites(points, l_x, l_y, N):
     NNgraph = utils.nearest_neighbors_graph(points=points, l_x=l_x, l_y=l_y, n_neighbors=4)
     psimn_vec = []
     nearest_neighbors = utils.nearest_neighbors(N=N, NNgraph=NNgraph)
-    for i in range(len(points)):
-        dr = [Metric.cyclic_vec([l_x, l_y], points[i], points[j]) for j in
-              nearest_neighbors[i]]
-        t = np.arctan2([r[1] for r in dr], [r[0] for r in dr])
-        psi_n = np.mean(np.exp(1j * n * t))
-        psimn_vec[i] = np.abs(psi_n) * np.exp(1j * self.m * np.angle(psi_n))
-    return psimn_vec
+    for i in range(N):
+        dr = [utils.cyclic_vec([l_x, l_y], points[i], points[j]) for j in nearest_neighbors[i]]
+        t = np.arctan2([np.abs(r[1]) for r in dr], [np.abs(r[0]) for r in dr])
+        psi_n = np.mean(np.exp(1j * 4 * t))
+        psimn_vec.append(np.abs(psi_n) * np.exp(1j * i * np.angle(psi_n)))
+    psi_avg = np.mean(psimn_vec)
+    orientation = np.imag(np.log(psi_avg))/4
+    return orientation
 
 
-def align_points(points, a):
-    theta = calculate_rotation_angle(points=points, a=a)
-    aligned_points = utils.rotate_points_by_angle(points,theta)
+def align_points(points, l_x, l_y, N):
+    theta = calculate_rotation_angel_averaging_on_all_sites(points=points, l_x=l_x, l_y=l_y , N=N)
+    aligned_points = utils.rotate_points_by_angle(points, theta)
     return aligned_points
 
 
@@ -79,10 +81,10 @@ def read_from_file():
     a = L / (np.sqrt(N) - 1)
     points = utils.read_points_from_file(file_path=file_path)
     assert points.shape == (N, 2)
-    aligned_points=align_points(points=points, a=a)
+    aligned_points = align_points(points=points, l_x=L, l_y=L, N=N)
     utils.plot_points_with_delaunay_edges_where_diagonals_are_removed(points=aligned_points, N=N, L=L)
 
 
-
 if __name__ == "__main__":
+    #demo()
     read_from_file()

post_process_2/utils.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
@@ -36,6 +37,21 @@ def rotation_matrix(theta):
 def rotate_points_by_angle(points, angle):
     rotated_points = points @ rotation_matrix(angle)
     return rotated_points
+
+
+def cyc_position_alignment(points, boundaries):
+    for p in points:
+        if p[0] > boundaries[0]:
+            p[0] = p[0] - boundaries[0]
+
+        if p[0] < 0:
+            p[0] = p[0] + boundaries[0]
+
+        if p[1] > boundaries[1]:
+             p[1] = p[1] - boundaries[1]
+
+        if p[1] < 0:
+             p[1] = p[1] + boundaries[1]
 
 
 def cyc_dist(p1, p2, boundaries):
@@ -82,6 +98,22 @@ def delaunay2edges(tri):
     return array_of_edges
 
 
+def NN2edges(points, nearest_neighbours, L):
+    list_of_edges = []
+    for p in range(len(nearest_neighbours)):
+        nn_edges = []
+        for n in range(len(nearest_neighbours[p])):
+            edge = (points[p], points[nearest_neighbours[p][n]])
+            if vec_length(edge[0],edge[1]) < L/2:
+                nn_edges.append(edge)
+        list_of_edges.append(nn_edges)
+    return list_of_edges
+
+
+def vec_length(p1,p2):
+    return math.dist(p1,p2)
+
+
 def nearest_neighbors_graph(points, l_x, l_y, n_neighbors):
     cyc = lambda p1, p2: cyc_dist(p1, p2, [l_x, l_y])
     NNgraph = kneighbors_graph(points, n_neighbors=n_neighbors, metric=cyc)
@@ -100,6 +132,20 @@ def get_lengths_of_edges(tri, array_of_edges):
         list_of_lengths.append((x1 - x2) ** 2 + (y1 - y2) ** 2)
     array_of_lengths = np.sqrt(np.array(list_of_lengths))
     return array_of_lengths
+
+
+def cyclic_vec(boundaries, sphere1, sphere2):
+    """
+    :return: vector pointing from sphere2 to sphere1 ("sphere1-sphere2") shortest through cyclic boundaries
+    """
+    dx = np.array(sphere1) - sphere2  # direct vector
+    vec = np.zeros(len(dx))
+    # vec[2] = dx[2]
+    for i in range(2):
+        l = boundaries[i]
+        dxs = np.array([dx[i], dx[i] + l, dx[i] - l])
+        vec[i] = dxs[np.argmin(dxs ** 2)]  # find shorter path through B.D.
+    return vec
 
 
 def filter_diagonal_edges(array_of_edges, a, points):
@@ -164,6 +210,17 @@ def plot_points_with_delaunay_edges_where_diagonals_are_removed(points, L, N):
     plot(points=points, edges_with_colors=edges_with_colors)
 
 
+def plot_nn_graph(nn_edges, points):
+    c=len(nn_edges[4])
+    for n in range(len(nn_edges)):
+        for e in range(len(nn_edges[n])):
+            x1, y1 = nn_edges[n][e][0]
+            x2, y2 = nn_edges[n][e][1]
+            plt.plot([x1, x2], [y1, y2],color='blue')
+    plt.scatter(points[:, 0], points[:, 1])
+    plt.show()
+
+
 def plot_points_with_delaunay_edges(points, L, N):
     # Perform Delaunay triangulation
     tri = Delaunay(points)
@@ -211,7 +268,6 @@ def get_L(N, h, rho_H):
     assert abs(l_x - l_y) < 0.000000001
     # l_z = (h + 1) * sig
     return l_x
-
 
 
 def read_points_from_file(file_path: str) -> np.ndarray: