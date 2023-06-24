fixed Burger field oriantaion and frustration calculation made much more efficent

post_process_2/burger_field_calculation.py
@@ -13,7 +13,6 @@ def Burger_field_calculation(points, l_x, l_y, N, global_theta, a):
     triangle_mid_points = tri.points[tri.simplices].mean(axis=1)
     no_of_triangles = len(tri.simplices)
     perfect_lattice_diagonal_vecs, perfect_lattice_non_diagonal_vecs = utils.perfect_lattice_vectors(a, 1)
-    # perfect_lattice_vecs = np.row_stack((perfect_lattice_non_diagonal_vecs, perfect_lattice_diagonal_vecs))
     aligned_perfect_lattice_diag_vecs = utils.rotate_points_by_angle(perfect_lattice_diagonal_vecs, global_theta)
     aligned_perfect_lattice_non_diag_vecs = utils.rotate_points_by_angle(perfect_lattice_non_diagonal_vecs,
                                                                          global_theta)
@@ -45,8 +44,8 @@ def is_not_zero(Burger_circut):
     return False
 
 
-def edge2vector(edge):
-    return np.array([edge[1] - edge[0]])
+def edge2vector(edge, points):
+    return np.array([points[edge[1]] - points[edge[0]]])
 
 
 def Burger_vector_calc(triangle_mid_point, Burger_circut):
@@ -62,12 +61,12 @@ def sort_triangle_edges_compared_to_reference_lattice(points, triangle,
 
     reference_lattice_vecs = np.row_stack((aligned_perfect_lattice_non_diag_vecs, aligned_perfect_lattice_diag_vecs))
 
-    ab = np.array([points[triangle[0]], points[triangle[1]]])
-    bc = np.array([points[triangle[1]], points[triangle[2]]])
-    ca = np.array([points[triangle[2]], points[triangle[0]]])
-    ab_ref = closest_reference_vector(edge2vector(ab), reference_lattice_vecs)
-    bc_ref = closest_reference_vector(edge2vector(bc), reference_lattice_vecs)
-    ca_ref = closest_reference_vector(edge2vector(ca), reference_lattice_vecs)
+    ab = np.array([triangle[0], triangle[1]])
+    bc = np.array([triangle[1], triangle[2]])
+    ca = np.array([triangle[2], triangle[0]])
+    ab_ref = closest_reference_vector(edge2vector(ab, points), reference_lattice_vecs)
+    bc_ref = closest_reference_vector(edge2vector(bc, points), reference_lattice_vecs)
+    ca_ref = closest_reference_vector(edge2vector(ca, points), reference_lattice_vecs)
 
     for e in [ab, bc, ca]:
         if utils.is_diagonal(e, aligned_perfect_lattice_diag_vecs, aligned_perfect_lattice_non_diag_vecs,points):

post_process_2/plot_points_with_delaunay_edges_where_diagonals_are_removed_after_rotation.py
@@ -82,7 +82,8 @@ def align_points(points, l_x, l_y, N, burger_vecs, theta):
 
 
 def read_from_file():
-    mac = False
+
+    mac = True
 
     if mac:
         file_path = "/Users/jalal/Desktop/ECMC/ECMC_simulation_results3.0/N=90000_h=0.8_rhoH=0.81_AF_square_ECMC/94363239"
@@ -94,24 +95,10 @@ def read_from_file():
     h = 0.8
     L, a, l_z = utils.get_params(N=N, h=h, rho_H=rho_H)
     # a = L / (np.sqrt(N) - 1)
+
     points_with_z = utils.read_points_from_file(file_path=file_path)
-
-    # N = 16
-    # L = 3
-    # l_z = 1
-    # a = 1
-    # noise = 0.2
-    # points_with_z = np.array([[0,3,0],[1,3.2,1],[2,3,0],[3,3,1],
-    #                           [0,2,1],[1.1,2,0],[2,2,1],[3,2,0],
-    #                           [0,1,0],[1,1.2,1],[2.2,0.9,0],[3,1,1],
-    #                           [0,0,1],[1,0,0],[2,0.4,1],[3,0,0]])
-
     points_z = points_with_z[:, 2]
     points = np.delete(points_with_z, 2, axis=1)
-
-    # noise = np.random.normal(0, noise, size=points.shape)
-    # points += noise
-
     assert points.shape == (N, 2)
     print("imported data and parameters")
     global_theta = calculate_rotation_angel_averaging_on_all_sites(points=points, l_x=L, l_y=L, N=N)
@@ -122,18 +109,9 @@ def read_from_file():
     rotated_Burger_vec = burger_vecs
     aligned_points_with_z = np.column_stack((aligned_points, points_z))
 
-    # utils.plot_points_with_delaunay_edges_where_diagonals_are_removed(points_with_z=aligned_points_with_z,
-    #                                                                   alignment_angel=global_theta,
-    #                                                                   burger_vecs=rotated_Burger_vec, a=a, l_z=l_z)
 
-    # i=0
-    # text_kwargs = dict(ha='center', va='center', fontsize=15, color='C1')
-    # for p in points:
-    #     plt.pyplot.text(p[0]+a/10, p[1]+a/10, i, **text_kwargs)
-    #     i=i+1
-
-    utils.plot(points=points, edges_with_colors=list_of_edges, burger_vecs=rotated_Burger_vec, non_diagonal=True)
-    #utils.plot_frustrations(list_of_edges, aligned_points_with_z, aligned_points, l_z)
+    utils.plot(points=aligned_points, edges_with_colors=list_of_edges, burger_vecs=rotated_Burger_vec, non_diagonal=True)
+    utils.plot_frustrations(list_of_edges, aligned_points_with_z, aligned_points, l_z)
     utils.plot_colored_points(aligned_points_with_z, l_z)
 
 

post_process_2/utils.py
@@ -66,7 +66,7 @@ def cyc_dist(p1, p2, boundaries):
 def is_horizontal(edge, points):
     x = np.array([[1, 0], [-1, 0]])
     y = np.array([[0, 1], [0, -1]])
-    vec = edge[1] - edge[0]
+    vec = points[edge[1]] - points[edge[0]]
     x_diff = get_closest_vector_in_length(vec, x)
     y_diff = get_closest_vector_in_length(vec, y)
     if x_diff < y_diff:
@@ -75,7 +75,7 @@ def is_horizontal(edge, points):
 
 
 def is_diagonal(edge, perfect_lattice_vectors_only_diags, perfect_lattice_vectors_only_no_diags, points) -> bool:
-    vec = edge[1] - edge[0]
+    vec = points[edge[1]] - points[edge[0]]
     x = get_closest_vector_in_length(vec, perfect_lattice_vectors_only_diags)
     y = get_closest_vector_in_length(vec, perfect_lattice_vectors_only_no_diags)
 
@@ -147,7 +147,7 @@ def plot_colored_points(points, l_z):
         else:
             plt.plot(p[0], p[1], 'bo',markersize=5)
 
-    plt.axis([160, 230, 340, 390])
+    plt.axis([130, 200, 360, 410])
     plt.gca().set_aspect('equal')
     plt.show()
 
@@ -156,11 +156,11 @@ def plot_frustrations(array_of_edges, points_with_z, points, l_z):
     print("coloring frustrations green")
     for (p1, p2), color in array_of_edges:
         if not (color == 'red'):
-            if (points_with_z[np.where(points == p1)[0][0]][2] > l_z/2 and points_with_z[np.where(points == p2)[0][0]][2] > l_z/2) or \
-               (points_with_z[np.where(points == p1)[0][0]][2] < l_z/2 and points_with_z[np.where(points == p2)[0][0]][2] < l_z/2):
+            if (points_with_z[p1][2] > l_z/2 and points_with_z[p2][2] > l_z/2) or \
+               (points_with_z[p1][2] < l_z/2 and points_with_z[p2][2] < l_z/2):
 
-                x1, y1 = p1[0], p1[1]
-                x2, y2 = p2[0], p2[1]
+                x1, y1 = points[p1]
+                x2, y2 = points[p2]
 
                 plt.plot([x1, x2], [y1, y2], color='green')
 
@@ -168,20 +168,23 @@ def plot_frustrations(array_of_edges, points_with_z, points, l_z):
 def plot(points, edges_with_colors, burger_vecs, non_diagonal):
     print("plotting edges")
     for (p1, p2), color in edges_with_colors:
-        x1, y1 = p1[0], p1[1]
-        x2, y2 = p2[0], p2[1]
-        if not(color == 'red' and non_diagonal==True):
+        x1, y1 = points[p1]
+        x2, y2 = points[p2]
+        if not(color == 'red' and non_diagonal):
             plt.plot([x1, x2], [y1, y2], color=color, alpha=1)
 
     print("plotting Burger field")
     if burger_vecs is not None:
-
-        # plt.quiver(burger_vecs[:, 0], burger_vecs[:, 1], burger_vecs[:, 2], burger_vecs[:, 3])
-
         for vec in burger_vecs:
-            dx = burger_vecs[2]-burger_vecs[0]
-            dy = burger_vecs[3]-burger_vecs[1]
-            plt.arrow(burger_vecs[0], burger_vecs[1], dx, dy)
+            dx = vec[2]-vec[0]
+            dy = vec[3]-vec[1]
+            norm = vector_length([dx, dy])
+            dx = dx/norm
+            dy = dy/norm
+            plt.arrow(vec[0]-dx/2, vec[1]-dy/2, dx/2, dy/2, head_width=0.4,
+                                                            head_length=0.7,
+                                                            length_includes_head=True,
+                                                            color='black')
 
 
 def plot_nn_graph(nn_edges, points):
@@ -190,7 +193,7 @@ def plot_nn_graph(nn_edges, points):
         for e in range(len(nn_edges[n])):
             x1, y1 = nn_edges[n][e][0]
             x2, y2 = nn_edges[n][e][1]
-            plt.plot([x1, x2], [y1, y2],color='blue')
+            plt.plot([x1, x2], [y1, y2], color='blue')
     plt.scatter(points[:, 0], points[:, 1])
     plt.show()
 
@@ -271,8 +274,7 @@ def plot_points_with_delaunay_edges_where_diagonals_are_removed(points_with_z, a
             edges_with_colors.append((e, "blue"))
 
     plot(points=points, edges_with_colors=edges_with_colors, burger_vecs=burger_vecs)
-
-    plot_frustrations(array_of_edges, points_with_z,l_z)
+    plot_frustrations(array_of_edges, points_with_z, l_z)
 
 
 def plot_points_with_no_edges(points):