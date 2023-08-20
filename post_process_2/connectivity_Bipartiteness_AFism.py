fixed some bugas added AF order parameter calculation

post_process_2/connectivity_Bipartiteness_AFism.py
@@ -1,21 +1,22 @@
 from post_process_2 import utils
 import matplotlib.pyplot as plt
 from post_process_2 import Graph
+import numpy as np
 
 
-def connectivity_Bipartiteness_AFism(list_of_edges, points, boundaries, theta):
+def connectivity_Bipartiteness_AFism(list_of_edges, points_with_z, boundaries, theta, l_z):
     G = Graph.Graph()
 
-    vertices, edges = keep_within_boundaries_and_non_isolated(G, list_of_edges, points, boundaries, theta)
+    vertices, edges = keep_within_boundaries_and_non_isolated(G, list_of_edges, points_with_z, boundaries, theta)
 
-    check_connectivity(G, vertices, edges)
-    check_bipartiteness(G, vertices, edges)
-    calculate_AF_order_parameter(G, vertices, edges)
+    visited = check_connectivity(G, vertices, edges)
+    vertices_sign = check_bipartiteness(G, vertices, edges, visited)
+    calculate_AF_order_parameter(G, vertices, edges, vertices_sign, visited, l_z)
 
     print("plotting edges")
     for (p1, p2) in edges:
-        x1, y1 = vertices[p1]
-        x2, y2 = vertices[p2]
+        x1, y1 = vertices[p1][1, 2]
+        x2, y2 = vertices[p2][1, 2]
 
         plt.plot([x1, x2], [y1, y2], color='grey', alpha=1)
         plt.plot(x1, y1, 'bo', markersize=5)
@@ -28,40 +29,45 @@ def connectivity_Bipartiteness_AFism(list_of_edges, points, boundaries, theta):
 
 
 def keep_within_boundaries_and_non_isolated(G, list_of_edges, points, boundaries, theta):
-    vertices = []
-    edges = []
 
-    first_rotation = utils.rotate_points_by_angle(points, -theta)
+    edges = []
+    N = len(points)
+    vertices = points
+    # vertices = np.delete(points, 2, axis=1)
+    # vertices = vertices.tolist()
+    # first_rotation = utils.rotate_points_by_angle(points_without_z, -theta)
 
     for (u, v), color, in_circuit in list_of_edges:
-
         if not (in_circuit or color == 'red'):
-            if not utils.out_of_boundaries(first_rotation[u], boundaries[0]):
-                vertices.append(points[u])
-                if not utils.out_of_boundaries(first_rotation[v], boundaries[1]):
-                    vertices.append(points[v])
-                    edges.append([len(vertices)-2, len(vertices)-1])
-                    G.addEdge(len(vertices)-2, len(vertices)-1)
-            else:
-                if not utils.out_of_boundaries(first_rotation[v], boundaries[1]):
-                    vertices.append(points[v])
+            if u < N and v < N:
+                G.addEdge(u, v)
+                edges.append([u, v])
+
+        # if not (in_circuit or color == 'red'):
+        #     if not utils.out_of_boundaries(first_rotation[u], boundaries[0]):
+        #         vertices.append(points[u])
+        #         if not utils.out_of_boundaries(first_rotation[v], boundaries[1]):
+        #             vertices.append(points[v])
+        #             edges.append([len(vertices)-2, len(vertices)-1])
+        #             G.addEdge(len(vertices)-2, len(vertices)-1)
+        #     else:
+        #         if not utils.out_of_boundaries(first_rotation[v], boundaries[1]):
+        #             vertices.append(points[v])
 
     return vertices, edges
 
 
 def check_connectivity(G, vertices, edges):
     i = 0
-    visited = []
-    print(min(G.graph))
-    visited[0] = G.BFS(0)
+    visited = [[G.BFS(0)]]
     node = not_all_visited(visited[i])
 
     while node != -1:
         i += 1
-        visited[i] = G.BFS()
+        visited.append(G.BFS(node))
         node = not_all_visited(visited[i])
 
-    print("Graph has", len(visited), "connected_componentes")
+    print("Graph has", len(visited), "connected componentes")
 
     return visited
 
@@ -74,9 +80,42 @@ def not_all_visited(visited):
     return -1
 
 
-def check_bipartiteness(G, vertices, edges):
-    pass
+def check_bipartiteness(G, vertices, edges, visited):
+    non_compatible, sign_0 = G.check_if_Bipartite_BFS(0)
+    sign = [[sign_0]]
+    print("non =", non_compatible)
+    for i in range(1, len(visited)):
+        node = not_all_visited(visited[i])
+        if node != -1:
+            sign.append(G.check_if_Bipartite_BFS(node)[1])
+
+    return sign
 
 
-def calculate_AF_order_parameter(G, vertices, edges):
-    pass
+def calculate_AF_order_parameter(G, vertices, edges, vertices_sign, visited, l_z):
+
+    order_parameter = 0
+    i = 0
+    while vertices_sign[0][i] == 0:
+        i += 1
+    if vertices[i][2] > l_z / 2:
+        up = vertices_sign[0][i]
+        down = -1 * vertices_sign[0][i]
+    else:
+        up = -1 * vertices_sign[0][i]
+        down = vertices_sign[0][i]
+
+    for component in vertices_sign:
+        for vertex in range(len(component)):
+            if component[vertex] != 0:
+                if vertices[vertex][2] > l_z/2:
+                    order_parameter += up * component[vertex]
+                else:
+                    order_parameter += down * component[vertex]
+
+    AF_order_parameter = order_parameter/len(vertices)
+
+    print("order parameter =", AF_order_parameter)
+
+    return AF_order_parameter
+

post_process_2/plot_points_with_delaunay_edges_where_diagonals_are_removed_after_rotation.py
@@ -56,6 +56,7 @@ def read_from_file():
     # a = L / (np.sqrt(N) - 1)
 
     points_with_z = utils.read_points_from_file(file_path=file_path)
+    unwrapped_aligned_points_z = points_with_z[:, 2]
     points = np.delete(points_with_z, 2, axis=1)
     assert points.shape == (N, 2)
     print("imported data and parameters")
@@ -69,12 +70,14 @@ def read_from_file():
     print("rotated points")
     print("theta=", global_theta)
     aligned_points_with_z = np.column_stack((aligned_points, wrapped_points_z))
+    unwrapped_aligned_points = utils.rotate_points_by_angle(points, global_theta)
+    unwrapped_aligned_points_with_z = np.column_stack((unwrapped_aligned_points, unwrapped_aligned_points_z))
 
     Burger_vecs, list_of_edges, is_point_in_dislocation = burger_field_calculation.Burger_field_calculation(points=aligned_points, a=a, order=1)
     print("no of total edges:", len(list_of_edges))
     optimized_Burgers_field = Burger_field_optimization.Burger_vec_optimization(aligned_points, list_of_edges, Burger_vecs, a, [L, L], global_theta)
 
-    connectivity_Bipartiteness_AFism.connectivity_Bipartiteness_AFism(list_of_edges, aligned_points, [L, L], global_theta)
+    connectivity_Bipartiteness_AFism.connectivity_Bipartiteness_AFism(list_of_edges, unwrapped_aligned_points_with_z, [L, L], global_theta, l_z)
 
     utils.plot_boundaries([L, L], global_theta)
     utils.plot_burger_field(optimized_Burgers_field, [L, L], True)